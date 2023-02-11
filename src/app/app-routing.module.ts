c34a5069456b2eda3ef8d31522cca0a76873f924
Wiktoria Stepien <stepwiktoriaa@gmail.com>
2023-02-12 00:11:15 +0100

    creating components,rounting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClassesComponent } from './modules/classes/classes.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { InstructorsComponent } from './modules/instructors/instructors.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'contact', component: ContactComponent
+
+  },
+
+  
+  {
+    path: 'instructors', component: InstructorsComponent
+
+  },
+
+
+  {
+    path: 'classes', component: ClassesComponent
+
+  },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,24 @@ import { AppComponent } from './app.component';
 import { AppBarComponent } from './components/app-bar/app-bar.component';
 import { AppFooterComponent } from './components/app-footer/app-footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactComponent } from './modules/contact/contact.component';
+import { InstructorsComponent } from './modules/instructors/instructors.component';
+import { ClassesComponent } from './modules/classes/classes.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AppBarComponent,
-    AppFooterComponent
+    AppFooterComponent,
+    ContactComponent,
+    InstructorsComponent,
+    ClassesComponent,
+   
+
+   
   ],
   imports: [
     BrowserModule,
